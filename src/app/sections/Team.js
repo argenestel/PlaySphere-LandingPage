9b53272ff2571e10977b22a59ac9c3f336d3f33a
argenestel <arpitpali68@gmail.com>
2024-06-12 20:07:39 +0530

    Teams page update

--- src/app/sections/Team.js
+++ src/app/sections/Team.js
@@ -27,17 +27,17 @@ const Team = () => {
       <div id='team' className='m-3 flex flex-wrap items-center justify-center rounded-[50px] bg-gradient-to-br from-pink-300 to-cyan-300 overflow-hidden pb-[50px]'>
         <div className='bg-gradient-to-r from-soft-pink to-soft-cyan text-black py-3'>
           <Marquee autoFill>
-            <h2 className='font-cc text-2xl md:text-5xl my-5 w-fit'>MEET THE TEAM ·&nbsp;</h2>
+            <h2 className='font-cc text-2xl md:text-5xl my-5 w-fit'>SNEAK PICK COMING SOON ·&nbsp;</h2>
           </Marquee>
         </div>
         <div className='w-full bg-black h-[7px]'>
 
         </div>
-        <div className='flex flex-wrap justify-center items-center'>
+        {/* <div className='flex flex-wrap justify-center items-center'>
           {team.map((e,ind)=>{
             return <TeamItem key={ind} image={e.image} name={e.name} position={e.position} />
           })}
-        </div>
+        </div> */}
         <Footer />
       </div>
     </>
